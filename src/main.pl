b99adf89f138cbc1f3e2d3646007712553c67cae
Konrad Claesson <konrad.claesson@gmail.com>
2019-11-17 00:50:49 +0100

    initial commit

--- src/main.pl
+++ src/main.pl
@@ -1,35 +1,106 @@
 verify(InputFileName) :-
   see(InputFileName),
-  read(Premises), read(Goal), read(Proof),
+  read(Premises), read(Conclusion), read(Proof),
   seen,
-  valid_proof(Premises, Goal, Proof).
-
-%% part of premises and goal
+  valid_proof(Premises, Conclusion, Proof).
 
 % The proof is valid if the proof ends with the sequent's
 % conclusion.
-valid_proof(Premises, Goal, Proof) :- 
+valid_proof(Premises, Conclusion, Proof) :- 
    writeln(Premises),
-   writeln(Goal),
+   writeln(Conclusion),
    writeln(Proof),
-   verify_end(Goal, Proof),
-   verify_proof(Premises, Proof).
+   verify_end(Conclusion, Proof),
+   verify_proof(Premises, Proof, []).
 
-% Verifies that proof ends with the sequent's conclusion 
-% (goal).
-verify_end(Goal, Proof) :-
-    last(Proof, Line),
-    nth0(1, Line, ProofConclusion),
-    Goal = ProofConclusion.
+% Verifies that proof ends with the sequent's conclusion.
+verify_end(Conclusion, Proof) :-
+    last(Proof, Line),              % line := last proof line
+    nth0(1, Line, ProofConclusion), % select ProofConclusion
+    Conclusion = ProofConclusion.
 
-% Proof = [H|T]
+% Recursively verifies the proof line by line.
+%
+% [H|T] = Proof
+% Verified:    previously verified proof lines
+% VerifiedNew: previously and newly verified proof lines
 verify_proof(_, []) :- !.
-verify_proof(Premises, [H|T]) :-
-   premise(Premises, H),
-   verify_proof(Premises, T).
+verify_proof(Premises, [H|T], Verified) :-
+   premise(Premises, H); 
+   rule(H, Verified),
+   append(Verified, H, VerifiedNew),
+   verify_proof(Premises, T, VerifiedNew).
 
 premise(_, []) :- !.
-premise(Premises, [1, Formula, premise]) :-
+premise(Premises, [_, Formula, premise]) :-
    member(Formula, Premises).
 
+% LAW OF EXCLUDED MIDDLE
+% NOT WORKING!!!
+rule([_, or(X, neg(X)), lem], _Verified).
+
+% COPY
+rule([_, X, copy(R)], Verified) :-
+   member([R, X, _], Verified).
+
+% DOUBLE NEGATION INTRODUCTION
+rule([_, neg(neg(X)), negnegint(R)], Verified) :-
+   member([R, X, _], Verified).
+
+% DOUBLE NEGATION ELIMINATION
+rule([_, X, negnegel(R)], Verified) :-
+   member([R, neg(neg(X)), _], Verified).
+
+% AND INTRODUCTION
+%
+% R1:       row number of formula X
+% R2:       row number of formula Y
+% Verified: currently verified proof lines
+rule([_, and(X, Y), andint(R1, R2)], Verified) :-
+   member([R1, X, _], Verified),
+   member([R2, Y, _], Verified).
+
+% 1st AND ELIMINATION
+rule([_, X, andel1(R)], Verified) :-
+   member([R, and(X, _), _], Verified).
+
+% 2nd AND ELIMINATION
+rule([_, Y, andel2(R)], Verified) :-
+   member([R, and(_, Y), _], Verified).
+
+% 1st OR INTRODUCTION
+rule([_, or(X, _), orint1(R)], Verified) :-
+   member([R, X, _], Verified).
+
+% 2nd OR INTRODUCTION
+rule([_, or(_, X), orint2(R)], Verified) :-
+   member([R, X, _], Verified).
+
+% OR ELIMINATION (requires assumptions)
+% rule([_, X, orel(R, S1, S2, T1, T2)], Verified) :-
+%    member([R, or(A, B), _], Verified),
+%    member([])
+
+% IMPLICATION INTRODUCTION
+
+% IMPLICATION ELIMINATION
+rule([_, Y, impel(R1, R2)], Verified) :-
+   member([R1, X, _], Verified),
+   member([R2, imp(X, Y), _], Verified).
+
+% NEGATION INTRODUCTION
+
+% NEGATION ELIMINATION
+rule([_, cont, negel(R1, R2)], Verified) :-
+   member([R1, X, _], Verified),
+   member([R2, neg(X), _], Verified).
+
+% CONTRADICTION ELIMINATION
+rule([_, _X, contel(R)], Verified) :-
+   member([R, cont, _], Verified).
+
+% MODUS TOLLENS
+rule([_, neg(X), mt(R1, R2)]) :-
+   member([R1, imp(X, Y), _], Verified),
+   member([R2, neg(Y), _], Verified).
 
